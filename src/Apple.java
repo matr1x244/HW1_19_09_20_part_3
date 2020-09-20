477fd06a2817376fcff584e0861b01340117a01a
matr1x244 <zio-21@yandex.ru>
2020-09-20 08:30:37 +0300

    вроде работает, но есть вопросы по классам apple и orange /* = почему то их подчёркивает как делали на уроке, и при исправлении ошибки в интеледжи меняет сам на super а из скобок убирает (float weight) - не понятно зачем так

--- src/Apple.java
+++ src/Apple.java
@@ -1,2 +1,12 @@
-public class Apple {
+public class Apple  extends  Fruit {
+
+    /* Если так начинает в майне ругаться на пустые () > Apple apple1 = new Apple ();
+    public Apple(float weight) {
+        super (1.0f);
+    }
+     */
+
+    public Apple() {
+        super (1.0f);
+    }
 }
--- src/Orange.java
+++ src/Orange.java
@@ -1,2 +1,12 @@
-public class Orange {
+public class Orange extends Fruit {
+
+    /* Если так начинает в майне ругаться на пустые () > Orange orange1 = new Orange ();
+    public Orange (float weight) {
+        super (1.5f);
+    }
+    */
+
+    public Orange() {
+        super (1.5f);
+    }
 }
